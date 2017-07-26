518b71f738b1790ad165c1ca9416c100c2dc51b4
ofisser86 <ofisser86@gmail.com>
2017-07-26 16:29:05 +0200

    Create a test form - result 200 OK

--- landing/views.py
+++ landing/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from .forms import SubscribersForm
 
 
 def landing(request):
-    return render(request, 'landind/landing.html', locals())
+    name = 'Dima'
+    form = SubscribersForm(request.POST or None)
+    if request.method == "POST" and form.is_valid():
+        data = form.cleaned_data
+        print(data['name'])
+
+        new_form = form.save()
+
+
+    return render(request, 'landing/landing.html', locals())
