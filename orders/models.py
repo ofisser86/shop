f90e356d2985945a2b7b150b98a4c7b18b4994ac
ofisser86 <ofisser86@gmail.com>
2017-07-31 12:10:19 +0200

    Make migrations and migrate products and orders models

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 from products.models import Product
 
@@ -8,7 +9,6 @@ class Status(models.Model):
     created = models.DateTimeField(auto_now_add=True,auto_now=False)
     updated = models.DateTimeField(auto_now_add=False,auto_now=True)
 
-
     def __str__(self):
         return 'Статус ИД %s ' % self.name
 
@@ -16,15 +16,15 @@ class Status(models.Model):
         verbose_name = 'Статус заказа'
         verbose_name_plural = 'Статусы заказов'
 
+
 class Order(models.Model):
-    cusromer_email = models.EmailField(blank=True, null=True, default=None)
+    customer_email = models.EmailField(blank=True, null=True, default=None)
     customer_name = models.CharField(max_length=64, blank=True, null=True, default=None)
-    cusromer_phone = models.CharField)(max_length=48, blank=True, null=True, default=None)
+    customer_phone = models.CharField(max_length=48, blank=True, null=True, default=None)
     comments = models.TextField(blank=True, null=True, default=None)
     is_active = models.ForeignKey(Status)
     created = models.DateTimeField(auto_now_add=True,auto_now=False)
     updated = models.DateTimeField(auto_now_add=False,auto_now=True)
-
 
     def __str__(self):
         return "Заказ %s %s " % (self.id, self.status.name)
@@ -39,7 +39,6 @@ class ProductInOrder(models.Model):
     product = models.ForeignKey(Product, blank=True, null=True, default=None)
     created = models.DateTimeField(auto_now_add=True,auto_now=False)
     updated = models.DateTimeField(auto_now_add=False,auto_now=True)
-
 
     def __str__(self):
         return "Продукт %s " % self.product.name
