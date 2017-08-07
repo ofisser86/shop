18ed70243607d4f87ad3038436c1757d03231098
ofisser86 <ofisser86@gmail.com>
2017-08-07 15:03:59 +0200

    Create product card (without slider and motion effects)

--- products/urls.py
+++ products/urls.py
@@ -13,8 +13,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
-from landing import views
+#from landing import views
+from products import views
 
 urlpatterns = [
-   # url(r'^landing/', views.landing, name='landing'),
+    url(r'^product/(?P<product_id>\w+)/$', views.product, name='product'),
 ]
